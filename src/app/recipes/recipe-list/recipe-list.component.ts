15ae6e579f9a0b187d234bbba84c68685332d46a
Yonatan Haddish <yonihailshn@gmail.com>
2023-11-18 14:47:02 -0500

    shoppingList / edit / recipe-list structured

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is a simple test',
+      'http://www.cartoonaday.com/images/cartoons/2011/11/thanksgiving-family-recipiep-cartoon-598x632.jpg'
+    ),
+    new Recipe(
+      'A Test Recipe',
+      'This is a simple test',
+      'http://www.cartoonaday.com/images/cartoons/2011/11/thanksgiving-family-recipiep-cartoon-598x632.jpg'
+    ),
+  ];
+  constructor() {}
 
+  ngOnInit() {}
 }
